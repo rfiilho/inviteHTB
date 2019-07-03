597c5464c08883266df203d7adfbd3a0f226b8de
Renato Filho <rfiilho@gmail.com>
2019-07-03 13:22:00 -0300

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,10 +1,10 @@
 xhr = new XMLHttpRequest()
 xhr.open("POST", "/api/invite/generate")
+xhr.onreadystatechange = function(){
+	if(xhr.readyState == 4 && xhr.status == 200){
+		r = JSON.parse(xhr.response);
+		r = atob(r.data.code)
+		alert("Invite Code is: "+ r)
+	}
+}
 xhr.send()
-
-setTimeout(function(){
-	r = JSON.parse(xhr.response);
-	r = atob(r.data.code)
-	alert("Invite Code is: "+ r)
-	
-}, 500);
